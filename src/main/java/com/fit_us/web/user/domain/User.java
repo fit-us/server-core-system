d9052a968099f1b1bfe88ebde40a81ff335cb969
KNU-K <knukang334@gmail.com>
2025-03-24 15:46:59 +0900

    refactor : User 와 Profile 연관관계 내에 cascade PERSIST 로 수정

--- src/main/java/com/fit_us/web/user/domain/User.java
+++ src/main/java/com/fit_us/web/user/domain/User.java
@@ -37,7 +37,7 @@ public class User extends BaseEntity {
     @Column(nullable = false, unique = true)
     private String email;
 
-    @OneToOne(fetch = FetchType.EAGER)
+    @OneToOne(cascade = CascadeType.PERSIST,fetch = FetchType.EAGER)
     private Profile profile;
 
     @Builder.Default
@@ -64,5 +64,18 @@ public class User extends BaseEntity {
             throw new IllegalStateException("User is already deleted.");
         }
         this.deletedAt = LocalDateTime.now();
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "name='" + name + '\'' +
+                ", nickname='" + nickname + '\'' +
+                ", oauthId='" + oauthId + '\'' +
+                ", provider='" + provider + '\'' +
+                ", email='" + email + '\'' +
+                ", profile=" + profile +
+                ", deletedAt=" + deletedAt +
+                '}';
     }
 }
