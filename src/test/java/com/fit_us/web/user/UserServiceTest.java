7ae734d907295d7fe9c190a7cb0197a501413a08
KNU-K <knukang334@gmail.com>
2025-03-24 15:56:36 +0900

    test : 유저 서비스 내의 유저 생성 로직 테스트 추가

---
+++ src/test/java/com/fit_us/web/user/UserServiceTest.java
@@ -0,0 +1,39 @@
+package com.fit_us.web.user;
+
+import com.fit_us.web.user.application.UserService;
+import com.fit_us.web.user.application.dto.CreateUserCommand;
+import com.fit_us.web.user.domain.User;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class UserServiceTest {
+    @Autowired
+    private UserService userService;
+    @Test
+    void 유저_생성_테스트(){
+        //given
+        CreateUserCommand.ProfileInfo profileInfo = new CreateUserCommand.ProfileInfo(
+                "url",
+                "2001-11-07",
+                "MALE",
+                "RH_PLUS_B"
+        );
+        CreateUserCommand command = new CreateUserCommand(
+                "testName",
+                "testNickname",
+                "testOauthId",
+                "testProvider",
+                "testEmail",
+                profileInfo
+        );
+        //when
+        User user = userService.create(command);
+
+        //then
+        Assertions.assertNotNull(user);
+        Assertions.assertNotNull(user.getId());
+    }
+}
