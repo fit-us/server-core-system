310c4759adbd5cb98eb8de0a3c65e2f479b64b08
KNU-K <knukang334@gmail.com>
2025-03-25 02:06:23 +0900

    test : UserRepository 를 통한 유저 생성에 따른 PK 부여 테스트

---
+++ src/test/java/com/fit_us/web/user/UserRepositoryTest.java
@@ -0,0 +1,42 @@
+package com.fit_us.web.user;
+
+import com.fit_us.web.user.domain.User;
+import com.fit_us.web.user.infrastructure.UserRepository;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.annotation.Rollback;
+
+@DataJpaTest
+@Slf4j
+public class UserRepositoryTest {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    private User user;
+    @BeforeEach
+    void setup(){
+        //Test 용 사용자 생성
+        user = User.create(
+                "testName",
+                "testNickname",
+                "testOauthId",
+                "testProvider",
+                "testEmail"
+        );
+    }
+    @Test
+    @Rollback
+    void 유저_생성에_따른_PK_부여(){
+        User savedUser = userRepository.save(user);
+
+        Assertions.assertNotNull(savedUser.getId());
+        Assertions.assertEquals(savedUser.getName(), user.getName());
+        Assertions.assertEquals(savedUser.getEmail(), user.getEmail());
+    }
+
+}
